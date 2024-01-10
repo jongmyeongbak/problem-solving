7f26b6970e724c2df9588f0c3342ec2faf0a7608
jongmyeongbak <116707497+jongmyeongbak@users.noreply.github.com>
2024-01-10 18:11:14 +0900

    uploaded at 2024. 1. 10. 오후 6:11:13

---
+++ programmers/문자열을 정수로 바꾸기/solution.java
@@ -0,0 +1,7 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12925?language=java
+
+class Solution {
+    public int solution(String s) {
+        return Integer.parseInt(s);
+    }
+}
