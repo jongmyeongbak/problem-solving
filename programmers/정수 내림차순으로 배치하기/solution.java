2cba277dc0b95a8abd964593924577cad64875d6
jongmyeongbak <116707497+jongmyeongbak@users.noreply.github.com>
2024-01-24 13:31:40 +0900

    uploaded at 2024. 1. 24. 오후 1:31:40

---
+++ programmers/정수 내림차순으로 배치하기/solution.java
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12933
+
+import java.util.*;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        int length = (int) Math.log10(n) + 1;
+        long[] numbers = new long[length];
+        for (int i = 0; n > 0; i++, n /= 10) {
+            numbers[i] = (long) n % 10;
+        }
+        Arrays.sort(numbers);
+        for (int i = length - 1; i >= 0; i--) {
+            answer = answer * 10 + numbers[i];
+        }
+        return answer;
+    }
+}
