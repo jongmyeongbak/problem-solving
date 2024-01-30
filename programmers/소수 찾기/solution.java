ac90326bf0112fec2ad05dfb466d9d512eb9b28a
jongmyeongbak <116707497+jongmyeongbak@users.noreply.github.com>
2024-01-30 20:57:10 +0900

    updated at 2024. 1. 30. 오후 8:57:10

--- programmers/소수 찾기/solution.java
+++ programmers/소수 찾기/solution.java
@@ -1,16 +1,24 @@
 // [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12921
 
 class Solution {
-    public int solution(int n) {
-        int answer = 1;
-        asc: for (int i = 3; i <= n; i += 2){
-            for (int j = 2; j * j <= i; j++){
-                if (i % j == 0) {
-                    continue asc;
-                }
-            }
+    public int solution(final int N) {
+        boolean[] notPrime = new boolean[N + 1];
+        
+        notPrime[0] = true;
+        notPrime[1] = true;
+        
+        int answer = 0;
+        
+        for (int i = 2; i <= N; i++) {
+            if (notPrime[i]) continue;
+            
             answer++;
+            
+            for (int idx = i + i; idx <= N; idx += i) {
+                notPrime[idx] = true;
+            }
         }
+        
         return answer;
     }
 }
